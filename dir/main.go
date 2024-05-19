0fa7cc0902195cdbc9d7964b4ac7aaadee2d1bbb
nametake <nametake.kyarabuki@gmail.com>
2024-05-20 01:12:06 +0900

    Added go mod

---
+++ dir/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"cloud.google.com/go/spanner"
+)
+
+func Foo() *spanner.Statement {
+	return &spanner.Statement{
+		SQL:    "SELECT * FROM TABLE;",
+		Params: map[string]interface{}{},
+	}
+}
